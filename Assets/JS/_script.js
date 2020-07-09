656e4c62ff8f303836347e3daf48aa12a47cbc07
Anirban Gorain <gorainanirban@gmail.com>
2020-07-09 23:15:27 +0530

    _add_Rain_Animation_Functionality

--- Assets/JS/_script.js
+++ Assets/JS/_script.js
@@ -24,3 +24,90 @@ function _mountain_Animation()
     requestAnimationFrame(_mountain_Animation);
     
 }
+
+// Rain.
+
+let _start_Or_Off = true;
+
+let _rain_Image = document.querySelector("._rain img");
+let _rain = document.querySelector("._rain");
+let _sky = document.querySelector("._sky");
+
+let _rain_Noise = new Audio;
+_rain_Noise.src = "/Assets/Musics/_rain_Noise.wav";
+_rain_Noise.volume = 0.2;
+_rain_Noise.loop = true;
+
+
+setInterval(() =>
+{
+
+    if (_start_Or_Off == true)
+    {
+
+        _rain_Image.setAttribute("src", "Assets/_images/_rain.png");
+        _rain_Image.style.opacity = 0;
+
+        let _time = 1;
+        let _opacity = 0.1;
+
+        _rain_Noise.play();
+
+        let _clear_Interval = setInterval(() =>
+        {
+
+
+            _rain_Image.style.opacity = _opacity;
+            _rain.style.background = `linear-gradient(180deg, rgb(0, 0, 0, ${_opacity-0.100}), rgba(0, 0, 255, 0))`;
+
+            _sky.style.filter = `grayscale(${_opacity * 50}%)`;
+
+            if (_time == 9)
+            {
+
+               clearInterval(_clear_Interval); 
+                
+            }
+
+            _time++;
+            _opacity += 0.1;
+            
+        }, 80);
+
+        _start_Or_Off = false;
+
+    }
+    else if (_start_Or_Off == false)
+    {
+
+        let _time = 1;
+        let _opacity = 0.9;
+
+        let _clear_Interval = setInterval(() =>
+        {
+
+
+            _rain_Image.style.opacity = _opacity;
+            _rain.style.background = `linear-gradient(180deg, rgb(0, 0, 0, ${_opacity-0.100}), rgba(0, 0, 255, 0))`;
+
+            _sky.style.filter = `grayscale(${_opacity * 50}%)`;
+
+            _rain_Noise.pause();
+
+            if (_time == 9)
+            {
+
+               clearInterval(_clear_Interval); 
+                
+            }
+
+            _time++;
+            _opacity -= 0.1;
+            
+        }, 80);
+
+        _start_Or_Off = true;
+        
+    }
+    
+}, 1000 * 60);
