0d570c1393e11ed519b77dad066beeca69f51fa2
Anirban Gorain <gorainanirban@gmail.com>
2020-07-13 11:50:23 +0530

    _add_The_Collision_Functionality

--- Assets/JS/_script.js
+++ Assets/JS/_script.js
@@ -446,13 +446,14 @@ document.addEventListener("keyup", (_event) =>
 const _opposite_Aeroplane = document.querySelector("#_opposite_Aeroplane");
 let _left = 4;
 let _to_Cancel_Animation_Frame;
+let _control_opposite_Aeroplane_Interval;
 
 let _danger_Sound = new Audio();
 _danger_Sound.src = "Assets/Musics/_danger_Alarm.mp3";
 _danger_Sound.volume = 0.2;
 _danger_Sound.loop = true;
 
-setInterval(() =>
+_control_opposite_Aeroplane_Interval = setInterval(() =>
 {
 
     _danger_Sound.play();
@@ -487,3 +488,270 @@ function _opposite_Aeroplane_Animation()
 
     
 }
+
+// Collision detection.
+
+// Down collision detection.
+
+let _aeroplane_Controller_2;
+let _down_Pipe_Container_2 = document.querySelector("._down_Pipe_Container");
+
+function _collision_Detector_Down()
+{
+    
+    _aeroplane_Controller_2 = document.querySelector("#_aeroplane_Controller").getBoundingClientRect();
+
+    // Pipe 1
+
+    if(!(_down_Pipe_Container_2.children[0].getBoundingClientRect().top > _aeroplane_Controller_2.bottom) && !(_down_Pipe_Container_2.children[0].getBoundingClientRect().left > _aeroplane_Controller_2.right) && !(_down_Pipe_Container_2.children[0].getBoundingClientRect().right < _aeroplane_Controller_2.left))
+    {
+
+        // Collision detected for pipe 1
+
+         _collision_Time();
+        
+
+    }
+
+    // Pipe 2
+
+    if(!(_down_Pipe_Container_2.children[1].getBoundingClientRect().top > _aeroplane_Controller_2.bottom) && !(_down_Pipe_Container_2.children[1].getBoundingClientRect().left > _aeroplane_Controller_2.right) && !(_down_Pipe_Container_2.children[1].getBoundingClientRect().right < _aeroplane_Controller_2.left))
+    {
+
+        // Collision detected for pipe 2.
+
+         _collision_Time();
+        
+
+    }
+
+    // Pipe 3
+
+    if(!(_down_Pipe_Container_2.children[2].getBoundingClientRect().top > _aeroplane_Controller_2.bottom) && !(_down_Pipe_Container_2.children[2].getBoundingClientRect().left > _aeroplane_Controller_2.right) && !(_down_Pipe_Container_2.children[2].getBoundingClientRect().right < _aeroplane_Controller_2.left))
+    {
+
+        // Collision detected for pipe 3.
+
+         _collision_Time();
+        
+
+    }
+
+    // Pipe 4
+
+    if(!(_down_Pipe_Container_2.children[3].getBoundingClientRect().top > _aeroplane_Controller_2.bottom) && !(_down_Pipe_Container_2.children[3].getBoundingClientRect().left > _aeroplane_Controller_2.right) && !(_down_Pipe_Container_2.children[3].getBoundingClientRect().right < _aeroplane_Controller_2.left))
+    {
+
+        // Collision detected for pipe 4.
+
+         _collision_Time();
+        
+
+    }
+
+    // Pipe 5
+
+    if(!(_down_Pipe_Container_2.children[4].getBoundingClientRect().top > _aeroplane_Controller_2.bottom) && !(_down_Pipe_Container_2.children[4].getBoundingClientRect().left > _aeroplane_Controller_2.right) && !(_down_Pipe_Container_2.children[4].getBoundingClientRect().right < _aeroplane_Controller_2.left))
+    {
+
+        // Collision detected for pipe 5.
+
+         _collision_Time();
+        
+
+    }
+
+    // Pipe 6
+
+    if(!(_down_Pipe_Container_2.children[5].getBoundingClientRect().top > _aeroplane_Controller_2.bottom) && !(_down_Pipe_Container_2.children[5].getBoundingClientRect().left > _aeroplane_Controller_2.right) && !(_down_Pipe_Container_2.children[5].getBoundingClientRect().right < _aeroplane_Controller_2.left))
+    {
+
+        // Collision detected for pipe 6.
+
+         _collision_Time();
+        
+
+    }
+
+    // Pipe 7
+
+    if(!(_down_Pipe_Container_2.children[6].getBoundingClientRect().top > _aeroplane_Controller_2.bottom) && !(_down_Pipe_Container_2.children[6].getBoundingClientRect().left > _aeroplane_Controller_2.right) && !(_down_Pipe_Container_2.children[6].getBoundingClientRect().right < _aeroplane_Controller_2.left))
+    {
+
+        // Collision detected for pipe 7.
+
+         _collision_Time();
+        
+
+    }
+
+}
+
+// Top or up collision detection.
+
+let _top_Pipe_Container_2 = document.querySelector("._top_Pipe_Container");
+
+function _collision_Detector_Up()
+{
+
+    // Pipe 1
+
+    if(!(_top_Pipe_Container_2.children[0].getBoundingClientRect().bottom < _aeroplane_Controller_2.top) &&  !(_down_Pipe_Container_2.children[0].getBoundingClientRect().left > _aeroplane_Controller_2.right) && !(_down_Pipe_Container_2.children[0].getBoundingClientRect().right < _aeroplane_Controller_2.left))
+    {
+
+        // Collision detected for pipe 1.
+        
+         _collision_Time();
+
+    }
+    if(!(_top_Pipe_Container_2.children[1].getBoundingClientRect().bottom < _aeroplane_Controller_2.top) &&  !(_down_Pipe_Container_2.children[1].getBoundingClientRect().left > _aeroplane_Controller_2.right) && !(_down_Pipe_Container_2.children[1].getBoundingClientRect().right < _aeroplane_Controller_2.left))
+
+    {
+
+        // Collision detected for pipe 2.
+        
+         _collision_Time();
+
+    }
+
+    if(!(_top_Pipe_Container_2.children[2].getBoundingClientRect().bottom < _aeroplane_Controller_2.top) &&  !(_down_Pipe_Container_2.children[2].getBoundingClientRect().left > _aeroplane_Controller_2.right) && !(_down_Pipe_Container_2.children[2].getBoundingClientRect().right < _aeroplane_Controller_2.left))
+
+    {
+
+        // Collision detected for pipe 3.
+        
+         _collision_Time();
+
+    }
+
+    if(!(_top_Pipe_Container_2.children[3].getBoundingClientRect().bottom < _aeroplane_Controller_2.top) &&  !(_down_Pipe_Container_2.children[3].getBoundingClientRect().left > _aeroplane_Controller_2.right) && !(_down_Pipe_Container_2.children[3].getBoundingClientRect().right < _aeroplane_Controller_2.left))
+
+    {
+
+        // Collision detected for pipe 4.
+        
+         _collision_Time();
+
+    }
+
+    if(!(_top_Pipe_Container_2.children[4].getBoundingClientRect().bottom < _aeroplane_Controller_2.top) &&  !(_down_Pipe_Container_2.children[4].getBoundingClientRect().left > _aeroplane_Controller_2.right) && !(_down_Pipe_Container_2.children[4].getBoundingClientRect().right < _aeroplane_Controller_2.left))
+
+    {
+
+        // Collision detected for pipe 5.
+        
+         _collision_Time();
+
+    }
+
+    if(!(_top_Pipe_Container_2.children[5].getBoundingClientRect().bottom < _aeroplane_Controller_2.top) &&  !(_down_Pipe_Container_2.children[5].getBoundingClientRect().left > _aeroplane_Controller_2.right) && !(_down_Pipe_Container_2.children[5].getBoundingClientRect().right < _aeroplane_Controller_2.left))
+
+    {
+
+        // Collision detected for pipe 6.
+        
+         _collision_Time();
+
+    }
+
+    if(!(_top_Pipe_Container_2.children[6].getBoundingClientRect().bottom < _aeroplane_Controller_2.top) &&  !(_down_Pipe_Container_2.children[6].getBoundingClientRect().left > _aeroplane_Controller_2.right) && !(_down_Pipe_Container_2.children[6].getBoundingClientRect().right < _aeroplane_Controller_2.left))
+
+    {
+
+        // Collision detected for pipe 7.
+        
+         _collision_Time();
+
+    }
+    
+}
+
+// Opposite aeroplane collision detection.
+
+function _collision_Detection_For_Opposite_Aeroplane()
+{
+
+    if(!(_aeroplane_Controller_2.right < _opposite_Aeroplane.getBoundingClientRect().left) && !(_aeroplane_Controller_2.left > _opposite_Aeroplane.getBoundingClientRect().right) && !(_aeroplane_Controller_2.top > _opposite_Aeroplane.getBoundingClientRect().bottom) && !(_aeroplane_Controller_2.bottom < _opposite_Aeroplane.getBoundingClientRect().top))
+    {
+
+        _collision_Time();
+
+    }
+    
+}
+
+// When will be collision what will be then.
+
+let _one_Time = true;
+
+// The main motive to declare the score variable here for the scope issue.
+
+let _score = 9;
+
+function _collision_Time()
+{
+
+    if(_one_Time == true)
+    {
+
+        document.querySelector("._combined_Aeroplane").classList.add("_collision_Animation");
+        document.querySelector("._fire_Container").style.visibility = "visible";
+
+        // Canceling the opposite aeroplane function caller interval.
+
+        clearInterval(_control_opposite_Aeroplane_Interval);
+
+        setTimeout(() =>
+        {
+
+            let _collision_Text = 
+            `
+
+                <div class="_collision_Text">
+
+                    <h1>Oops, ${localStorage.getItem("_name")} you loose this round</h1> 
+                    <h2>You May try again further</h2> 
+                    <h2><span id="_score">${(_score >= parseInt(localStorage.getItem("_highest_Score")))? `You made a new high score which is ${_score}`: `Your score is ${_score}, And the high score is ${localStorage.getItem("_highest_Score")}`}</span></h2>
+                    <button class="_button">PLAY Again</button>
+                    <a href="#here will be come the main page" target="_blank">Go to the main page.</a>
+
+                </div>
+
+            `;
+
+            // Collision text.
+
+            let _collision_Text_Container = document.createElement("div");
+            document.querySelector("body").appendChild(_collision_Text_Container);
+            _collision_Text_Container.innerHTML = _collision_Text;
+            _collision_Text_Container.setAttribute("class", "_collision_Text");
+            
+
+
+            document.querySelector("._combined_Aeroplane").style.visibility = "hidden";
+            document.querySelector("._fire_Container").style.visibility = "hidden";
+            
+        }, 1000 * 5);
+
+        _one_Time = false;
+
+    }
+
+    
+    
+}
+
+function _execute_Collision_Checkers_Functions_Infinite_Time()
+{
+
+    _collision_Detector_Down();
+    _collision_Detector_Up();
+
+    _collision_Detection_For_Opposite_Aeroplane();
+
+    requestAnimationFrame(_execute_Collision_Checkers_Functions_Infinite_Time);
+
+}
+
+_execute_Collision_Checkers_Functions_Infinite_Time();
+
