4a7cc49a7c335a290700325fb379a4686c9736f3
Anirban Gorain <gorainanirban@gmail.com>
2020-07-15 07:24:07 +0530

    _add_Scroll_Time_Navbar_Background_Color_Change

--- Assets/JS/_landing_Page.js
+++ Assets/JS/_landing_Page.js
@@ -38,3 +38,44 @@ document.onload
     });
 
 }
+
+// Scroll time navbar color changing.
+
+const _navbar = document.querySelector("nav");
+const _anchor = document.querySelectorAll("nav ul li a");
+const _logo = document.querySelector("nav div")
+
+window.addEventListener("scroll", () =>
+{
+
+    if(this.scrollY > 10)
+    {
+
+        _navbar.style.backgroundColor = "rgb(46, 43, 46)";
+        _logo.style.color = "white";
+        
+        for(let _index = 0; _index < _anchor.length; _index++)
+        {
+
+            _anchor[_index].style.color = "white";
+
+        }
+
+    }
+    else
+    {
+
+        _navbar.style.backgroundColor = "white";
+        _logo.style.color = "rgb(46, 43, 46)";
+        
+        for(let _index = 0; _index < _anchor.length; _index++)
+        {
+
+            _anchor[_index].style.color = "black";
+
+        }
+
+    }
+
+    
+});
